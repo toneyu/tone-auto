c727aa80b488ba7991c5f3d6ba55dbc367923954
Tone Yu <tone.yu@accenture.com>
2019-12-08 00:42:06 -0600

    update tab icons

--- app/containers/TabsContainer.jsx
+++ app/containers/TabsContainer.jsx
@@ -5,49 +5,56 @@ import WifiIcon from '@material-ui/icons/Wifi';
 import { push } from 'connected-react-router';
 import { connect } from 'react-redux';
 import Paper from '@material-ui/core/Paper';
+import AppsIcon from '@material-ui/icons/Apps';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
 
 import routes from '../constants/routes';
 import ConnectionStatus from '../constants/connection-status';
 
-const TabsContainer = ({ pathname, push, connectionStatus }) => (
-  <Paper square>
-    <Tabs
-      value={pathname}
-      indicatorColor="primary"
-      textColor="primary"
-      // onChange={handleChange}
-      aria-label="disabled tabs example"
-      onChange={(_, value) => push(value)}
-    >
-      <Tab
-        icon={
-          connectionStatus === ConnectionStatus.CONNECTED ? (
-            <WifiIcon color="primary" />
-          ) : connectionStatus === ConnectionStatus.CONNECTING ? (
-            <CircularProgress />
-          ) : (
-            <WifiOffIcon color="error" />
-          )
-        }
-        label="Connect"
-        value={routes.CONNECTIONS}
-      />
-      <Tab
-        icon={<WifiOffIcon />}
-        label="Commands"
-        value={routes.COMMANDS}
-        disabled={connectionStatus !== ConnectionStatus.CONNECTED}
-      />
-      <Tab
-        icon={<WifiOffIcon />}
-        label="Scripts"
-        value={routes.SCRIPTS}
-        disabled={connectionStatus !== ConnectionStatus.CONNECTED}
-      />
-    </Tabs>
-  </Paper>
-);
+const TabsContainer = ({ pathname, push, connectionStatus }) => {
+  const connected = connectionStatus === ConnectionStatus.CONNECTED;
 
+  return (
+    <Paper square>
+      <Tabs
+        value={pathname}
+        indicatorColor="primary"
+        textColor="primary"
+        // onChange={handleChange}
+        aria-label="disabled tabs example"
+        onChange={(_, value) => push(value)}
+      >
+        <Tab
+          icon={
+            connectionStatus === ConnectionStatus.CONNECTED ? (
+              <WifiIcon style={{ color: 'green' }} />
+            ) : connectionStatus === ConnectionStatus.CONNECTING ? (
+              <CircularProgress />
+            ) : (
+              <WifiOffIcon color="error" />
+            )
+          }
+          label="Connect"
+          value={routes.CONNECTIONS}
+        />
+        <Tab
+          icon={<AppsIcon />}
+          label="Commands"
+          color={connected ? 'primary' : 'disabled'}
+          value={routes.COMMANDS}
+          disabled={!connected}
+        />
+        <Tab
+          icon={<FormatListNumberedIcon />}
+          label="Scripts"
+          value={routes.SCRIPTS}
+          color={connected ? 'primary' : 'disabled'}
+          disabled={!connected}
+        />
+      </Tabs>
+    </Paper>
+  );
+};
 const mapStateToProps = (state) => ({
   pathname: state.router.location.pathname,
   connectionStatus: state.connection.status,
