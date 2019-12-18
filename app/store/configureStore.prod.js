e17e6c83844069098d930e60d6ac46719f654a6c
Tone Yu <tone.yu@accenture.com>
2019-12-18 01:07:57 -0600

    Added Redux to production

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,18 +1,74 @@
-// @flow
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
-import { routerMiddleware } from 'connected-react-router';
+import { routerMiddleware, routerActions } from 'connected-react-router';
+import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
 import createRootReducer from '../reducers';
 import type { counterStateType } from '../reducers/types';
+import rootSaga from '../sagas';
 
 const history = createHashHistory();
-const rootReducer = createRootReducer(history);
-const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
 
-function configureStore(initialState?: counterStateType) {
-  return createStore<*, counterStateType, *>(rootReducer, initialState, enhancer);
-}
+const rootReducer = createRootReducer(history);
+
+const configureStore = (initialState?: counterStateType) => {
+  const sagaMiddleware = createSagaMiddleware({});
+
+  // Redux Configuration
+  const middleware = [sagaMiddleware];
+  const enhancers = [];
+
+  // Thunk Middleware
+  middleware.push(thunk);
+
+  // Logging Middleware
+  const logger = createLogger({
+    level: 'info',
+    collapsed: true,
+  });
+
+  // Skip redux logs in console during the tests
+  if (process.env.NODE_ENV !== 'test') {
+    middleware.push(logger);
+  }
+
+  // Router Middleware
+  const router = routerMiddleware(history);
+  middleware.push(router);
+
+  // Redux DevTools Configuration
+  const actionCreators = {
+    ...routerActions,
+  };
+  // If Redux DevTools Extension is installed use it, otherwise use Redux compose
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Options: http://extension.remotedev.io/docs/API/Arguments.html
+        actionCreators,
+      })
+    : compose;
+  /* eslint-enable no-underscore-dangle */
+
+  // Apply Middleware & Compose Enhancers
+  enhancers.push(applyMiddleware(...middleware));
+  const enhancer = composeEnhancers(...enhancers);
+
+  // Create Store
+  const store = createStore(rootReducer, initialState, enhancer);
+
+  sagaMiddleware.run(rootSaga);
+
+  if (module.hot) {
+    module.hot.accept(
+      '../reducers',
+      // eslint-disable-next-line global-require
+      () => store.replaceReducer(require('../reducers').default),
+    );
+  }
+
+  return store;
+};
 
 export default { configureStore, history };
