7f084149e2670db5ebae0e2ac2df95700d60a517
Tone Yu <tone.yu@accenture.com>
2020-04-18 17:20:17 -0500

    remove unused code

--- app/containers/ConnectionPanel.jsx
+++ app/containers/ConnectionPanel.jsx
@@ -1,31 +1,13 @@
 import { AccordionPanel, Box } from 'grommet';
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import ConnectionPanelHeader from './ConnectionPanelHeader';
-import ConnectionStatus from '../constants/connection-status';
 import Connection from './Connection';
-
-const usePrev = (value) => {
-  const ref = useRef();
-
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
-
-  return ref.current;
-};
 
 const ConnectionPanel = ({ connection }) => {
   const connectionStatus = useSelector(
     (state) => state.connection.entities[connection.host]?.status,
   );
-  const prevConnectionStatus = usePrev(connectionStatus);
-  if (
-    connectionStatus !== prevConnectionStatus &&
-    connectionStatus === ConnectionStatus.CONNECTED
-  ) {
-    // addActivePanel();
-  }
 
   return (
     <AccordionPanel
